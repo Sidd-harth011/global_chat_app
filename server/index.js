f68e60d93e8ad808b9442a1fad0e03c0f67e90e9
Sidd-harth011 <siddharthgautam573@gmail.com>
2025-08-18 01:09:57 +0530

    added readme and modified package.json

--- server/index.js
+++ server/index.js
@@ -45,3 +45,4 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+//////////////////////////////////////////////fffforr checking the push
